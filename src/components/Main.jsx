afa957dbd942954c72e20c6ce9915d641366d12e
Stiven hoxha <stivenhoxha007@gmail.com>
2025-02-19 17:35:36 +0100

    lista array

---
+++ src/components/Main.jsx
@@ -0,0 +1,78 @@
+import {userState} from 'react';
+
+const materieScolastiche = [
+    {
+        id: 1,
+        titolo: "storia",
+        autore: "autore 1",
+        contenuto: "per non dimenticare",
+        categoria: "II guerra mondiale"
+    },
+
+    {
+        id: 2,
+        titolo: "letteratura",
+        autore: "autore 2",
+        contenuto: "scritti",
+        categoria: "dell'900" 
+    },
+
+    {
+        id: 3,
+        titolo: "filosofia",
+        autore: "autore 3",
+        contenuto: "il valore della vita",
+        categoria: "chi siamo?"
+    },
+
+    {
+        id: 4,
+        titolo: "matematica",
+        autore: "autore 4",
+        contenuto: "problemi",
+        categoria: "probabilità"
+    },
+
+    {
+        id: 5,
+        titolo: "fisica",
+        autore: "autore 5",
+        contenuto: "vettori",
+        categoria: "esercizi"
+    },
+
+    {
+        id: 6,
+        titolo: "geografia",
+        autore: "autore 6",
+        contenuto: "clima",
+        categoria: ""
+    },
+
+    {
+        id: 7,
+        titolo: "psicologia",
+        autore: "autore 7",
+        contenuto: "psiche umana",
+        categoria: "traumi"
+    },
+
+    {
+        id: 8,
+        titolo: "chimica",
+        autore: "autore 8",
+        contenuto: "nomenclatura",
+        categoria: "formule"
+    }
+];
+
+const initialFormData = {
+    titolo: "",
+    autore: "",
+    contenuto: "",
+    categoria: ""
+}
+
+export default function subjectsList() {
+    
+}
